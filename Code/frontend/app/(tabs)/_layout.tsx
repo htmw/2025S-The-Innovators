0a5f5229a7edc54a1dd541e0a047878d93e35c8c
rd39257n <rd39257n@pace.edu>
2025-03-10 02:52:34 -0400

    removal of explore code

--- Code/frontend/app/(tabs)/_layout.tsx
+++ Code/frontend/app/(tabs)/_layout.tsx
@@ -1,32 +1,23 @@
+import { Tabs } from 'expo-router';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Platform } from 'react-native';
-import HomeScreen from './HomeScreen';
-
-const Tab = createBottomTabNavigator();
 
 export default function TabLayout() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: Platform.select({
-            ios: {
-              position: 'absolute',
-            },
-            default: {},
-          }),
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: 'Home',
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#3498db',
+        tabBarStyle: {
+          display: 'none', // Hide the tab bar completely
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+    </Tabs>
   );
 }
